f45a033bc41e4f6bbb53e90abb7e7e600cb76a96
shun <shun@isasaka.jp>
2019-02-11 16:35:11 +0900

    ファイルを読み込めたぞ

---
+++ app/src/main/java/com/toshiki/shun/gnlivewallpaper/GNLiveWallpaper.kt
@@ -0,0 +1,12 @@
+package com.toshiki.shun.gnlivewallpaper
+
+import android.service.wallpaperWallpaperService
+
+/**
+ * GNLiveWallpaper
+ *
+ * ライブ壁紙のメインクラス
+ */
+class GNLiveWallpaper : WallpaperService() {
+
+}
