08c6a45b3ee9e2607c4a20727a029b2ae655d52f
shun <shun@isasaka.jp>
2019-02-11 17:25:39 +0900

    いらないところを削る

--- app/src/main/java/com/toshiki/shun/gnlivewallpaper/GNLiveWallpaper.kt
+++ app/src/main/java/com/toshiki/shun/gnlivewallpaper/GNLiveWallpaper.kt
@@ -1,13 +1,15 @@
 package com.toshiki.shun.gnlivewallpaper
 
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.graphics.Paint
 import android.os.Handler
 import android.service.wallpaper.WallpaperService
 import android.util.Log
 import android.view.SurfaceHolder
 import java.io.File
+
+
 
 /**
  * ライブ壁紙メインクラスです。
@@ -32,18 +34,6 @@ class GNLiveWallpaper : WallpaperService() {
     inner class LiveWallpaperEngine : Engine() {
 
         /**
-         * ライブ壁紙起動時の時刻
-         */
-        private var mFirstTime = 0L
-        /**
-         * 計測開始時(fps調整用)
-         */
-        private var mStartTime = 0L
-        /**
-         * 計測終了時(fps調整用)
-         */
-        private var mEndTime = 0L
-        /**
          * 描画領域(幅)
          */
         private var mWidth = 0
@@ -55,10 +45,8 @@ class GNLiveWallpaper : WallpaperService() {
          * ライブ壁紙が表示されているか
          */
         private var mVisible = false
-        /**
-         * 表示する画像番号
-         */
-        private var mIndex = "0000"
+        private val mPaint = Paint()
+
 
         init {
         }
@@ -94,47 +82,14 @@ class GNLiveWallpaper : WallpaperService() {
          * 画面に書き込みます。
          */
         private fun drawFrame() {
-            if (mFirstTime == 0L) {
-                mFirstTime = System.currentTimeMillis()
-            }
-            mStartTime = System.currentTimeMillis()
-
             val canvas = surfaceHolder.lockCanvas()
-            canvas?.let { canvas ->
-                drawPicture(canvas)
-                surfaceHolder.unlockCanvasAndPost(canvas)
-            }
-
-            mEndTime = System.currentTimeMillis()
-
             // 次の描画設定
-            val delay = 1000 / FPS - (mEndTime - mStartTime)
+            val delay = 1000L / FPS
             mHandler.removeCallbacks(mDrawRunnable)
             if (mVisible) {
                 mHandler.postDelayed(mDrawRunnable, delay)
             }
         }
-
-        /**
-         * 画像をCanvasに描画します。
-         *
-         * @param canvas キャンバス
-         */
-        private fun drawPicture(canvas: Canvas) {
-            try {
-                // 現在表示すべき画像番号を計算
-                val index = (mStartTime - mFirstTime) / (1000 / FPS) % 227
-                val indexString = String.format("%04d", index)
-                // assets から画像を読み込み、 canvas に描画
-                assets.open("animation${File.separator}live_wallpaper_test$indexString.jpg").use { inputStream ->
-                    val bitmap = BitmapFactory.decodeStream(inputStream)
-                    canvas.drawBitmap(bitmap, null, Rect(0, 0, mWidth, mHeight), null)
-                }
-            } catch (e: Throwable) {
-                Log.e("LiveWallpaperTest", e.message, e)
-            }
-        }
-
         /**
          * 画面書き込み用スレッド
          */
